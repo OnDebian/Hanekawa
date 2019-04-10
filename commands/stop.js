2e2794deca1b6fee0b313d8de713c5afc842be2c
OnDebian <ondebian@protonmail.com>
2019-04-11 00:44:03 +0200

    patch stop command

--- commands/stop.js
+++ commands/stop.js
@@ -14,7 +14,7 @@ module.exports = {
         if(!message.member.voiceChannel) return message.reply("You are not in a voice channel");
         if(!message.guild.member(client.user).voiceChannel) return message.reply(`${message.guild.member(client.user).displayName} is not in a voice channel`);
         if(player.getQueue(message.guild.id).length > 0) player.emptyQueue(message.guild.id);
-        (!message.guild.member(client.user).voiceChannel.connection)
+        (message.guild.member(client.user).voiceChannel.connection)
             ? message.guild.member(client.user).voiceChannel.connection.dispatcher.end()
             : message.guild.member(client.user).voiceChannel.leave();
         message.channel.send(
